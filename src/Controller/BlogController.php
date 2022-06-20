f489ab0ab465948519d5e78aa40286c27711369c
Baptiste <bda@emakina.fr>
2022-06-20 18:18:01 +0200

    Blog avec liste des article opérationnelle

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,19 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+// use Doctrine\ORM\EntityManagerInterface;
 
 class BlogController extends AbstractController
 {
+
     #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        // Retrieves a repository managed by the "default" entity manager
+        $articles = $doctrine->getRepository(Article::class)->findAll();
+
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
-                'title' => "Bienvenue Guillaume",
-                'age' => 28
+                'articles' => $articles
         ]);
     }
 
@@ -24,10 +31,13 @@ class BlogController extends AbstractController
         return $this->render('blog/home.html.twig');
     }
 
-    #[Route('/blog/12', name: 'blog_show')]
-    public function show() : Response
+    #[Route('/blog/{id}', name: 'blog_show')]
+    public function show(ManagerRegistry $doctrine,$id) : Response
     {
-        return $this->render('blog/show.html.twig');
+        $article = $doctrine->getRepository(Article::class)->find($id);
+        return $this->render('blog/show.html.twig',[
+            'article' => $article
+        ]);
     }
 
 }
