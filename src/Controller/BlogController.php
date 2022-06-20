c2f1044c59f3dd2002ed4c0fdba041229118cc5d
Baptiste <bda@emakina.fr>
2022-06-20 10:42:57 +0200

    Je montre

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -23,4 +23,11 @@ class BlogController extends AbstractController
     {
         return $this->render('blog/home.html.twig');
     }
+
+    #[Route('/blog/12', name: 'blog_show')]
+    public function show() : Response
+    {
+        return $this->render('blog/show.html.twig');
+    }
+
 }
