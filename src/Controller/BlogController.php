1fc2174aecdc5cdae2c6129d5735ce594a454d9e
Baptiste <bda@emakina.fr>
2022-06-21 11:43:21 +0200

    Création d'article fonctionnelle

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,11 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use phpDocumentor\Reflection\Types\This;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
-// use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormTypeInterface;
 
 class BlogController extends AbstractController
 {
@@ -31,6 +38,46 @@ class BlogController extends AbstractController
         return $this->render('blog/home.html.twig');
     }
 
+    #[Route('/blog/new', name:'blog_create')]
+    public function create(Request $request, EntityManagerInterface $manager){
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+                     ->add('title',TextType::class,[
+                         'attr' => [
+                             'placeholder' => "Titre de l'article",
+                             'class' => "form-control"
+                         ]
+                     ])
+                     ->add('content',TextareaType::class,[
+                         'attr' => [
+                             'placeholder' => "Contenu de l'article",
+                             'class' => "form-control"
+                         ]
+                     ])
+                     ->add('image',TextType::class,[
+                         'attr' => [
+                             'placeholder' => "Image de l'article",
+                             'class' => "form-control"
+                         ]
+                     ])
+                     ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $article->setCreatedAt(new \DateTimeImmutable());
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show',['id' => $article->getId()]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView()
+        ]);
+    }
+
     #[Route('/blog/{id}', name: 'blog_show')]
     public function show(ManagerRegistry $doctrine,$id) : Response
     {
@@ -39,5 +86,6 @@ class BlogController extends AbstractController
             'article' => $article
         ]);
     }
+
 
 }
